b8a3db2ec12c97b433cb487ad5829f42e58288b4
Chris Whitney <drifterz28@users.noreply.github.com>
2020-02-08 23:13:16 -0800

    Adding auth

--- arduino/esp-temp-track/esp-temp-track.ino
+++ arduino/esp-temp-track/esp-temp-track.ino
@@ -1,1 +1,36 @@
-// soon
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoJson.h>
+#include "FS.h"
+
+ESP8266WiFiMulti WiFiMulti;
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "hello from esp8266!");
+}
+
+void setup(void) {
+  Serial.begin(115200);
+  SPIFFS.begin();
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP("ssid", "password");
+  Serial.println("");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+  
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+  
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void) {
+server.handleClient();  
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,56 @@ require('dotenv').config();
 
 const accountSid = process.env.SSID;
 const authToken = process.env.TOKEN;
+const port = process.env.PORT || 1337;
+
 const http = require('http');
 const express = require('express');
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const port = process.env.PORT || 1337;
-const app = express();
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const session = require('express-session');
+
 const {query} = require('./lib/connect.js');
+
+const app = express();
+// config express-session
+const sess = {
+  secret: 'CHANGE THIS TO A RANDOM SECRET',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true
+};
+
+const strategy = new Auth0Strategy(
+  {
+    domain: process.env.AUTH0_DOMAIN,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+    callbackURL:
+      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  }
+);
+
+passport.use(strategy);
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session(sess));
 
 app.get('/', async (req, res) => {
   const [rows,fields] = await query("SELECT 1");
@@ -28,7 +71,10 @@ app.get('/', async (req, res) => {
   //   });
 });
 
-app.get('/report', )
+app.post('/report/:token/:temp/:battery', async (req, res) => {
+  const { params } = req;
+  const dbResults = await query('INSERT INTO temps ', );
+});
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
@@ -38,6 +84,10 @@ app.post('/sms', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
+
+app.get('/health' (_, res) => {
+  res.json({'health': 'good'});
+})
 
 http.createServer(app).listen(port, () => {
   console.log(`Express server listening on port ${port}`);
