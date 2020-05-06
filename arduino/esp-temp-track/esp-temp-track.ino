f3480d9d33acfcfbfd59fcdce46135c05e88a951
Chris Whitney <cwhitney@kollective.com>
2020-05-05 20:43:47 -0700

    Setting up arduino code

--- arduino/esp-temp-track/esp-temp-track.ino
+++ arduino/esp-temp-track/esp-temp-track.ino
@@ -1,188 +1,70 @@
-#include <FS.h>                   //this needs to be first, or it all crashes and burns...
-#include <math.h>
 #include "ConfigManager.h"
+#include <math.h>
 
-#define THERMISTORPIN A0         
-// resistance at 25 degrees C
-#define THERMISTORNOMINAL 10000      
-// temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25   
-// how many samples to take and average, more takes longer
-// but is more 'smooth'
-#define NUMSAMPLES 5
-// The beta coefficient of the thermistor (usually 3000-4000)
-#define BCOEFFICIENT 3950
-// the value of the 'other' resistor
-#define SERIESRESISTOR 1000
-int samples[NUMSAMPLES];
-//flag for saving data
-bool shouldSaveConfig = false;
-ESP8266WebServer server(80);
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-  Serial.println("Should save config");
-  shouldSaveConfig = true;
+const char *settingsHTML = (char *)"/settings.html";
+const char *mainJS = (char *)"/index.js";
+
+struct Config {
+  char phone[10];
+  char key[64];
+  float temp;
+  float timing;
+} config;
+
+struct Metadata {
+  int8_t version;
+} meta;
+
+ConfigManager configManager;
+
+void APCallback(WebServer *server) {
+    DebugPrintln(F("AP Mode Enabled. You can call other functions that should run after a mode is enabled ... "));
+}
+
+
+void APICallback(WebServer *server) {
+  server->on("/disconnect", HTTPMethod::HTTP_GET, [server](){
+    configManager.clearWifiSettings(false);
+  });
+
+  server->on("/setup.html", HTTPMethod::HTTP_GET, [server](){
+    configManager.streamFile(settingsHTML, mimeHTML);
+  });
+
+  server->on("/index.js", HTTPMethod::HTTP_GET, [server](){
+    configManager.streamFile(mainJS, mimeJS);
+  });
+
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  DEBUG_MODE = true;
   Serial.begin(115200);
-  Serial.println();
+  DebugPrintln(F(""));
 
-  //clean FS, for testing
-  //SPIFFS.format();
+  meta.version = 3;
 
-  //read configuration from FS json
-  Serial.println("mounting FS...");
+  // Setup config manager
+  configManager.setAPName("Demo");
+  configManager.setAPFilename("/setup.html");
 
-  if (SPIFFS.begin()) {
-    Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json")) {
-      //file exists, reading and loading
-      Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile) {
-        Serial.println("opened config file");
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
+  // Settings variables
+  configManager.addParameter("phone", config.phone, 10);
+  configManager.addParameter("key", config.key, 64);
+  configManager.addParameter("temp", &config.temp);
+  configManager.addParameter("timing", &config.timing);
 
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonBuffer jsonBuffer;
-        JsonObject& json = jsonBuffer.parseObject(buf.get());
-        json.printTo(Serial);
-        if (json.success()) {
-          Serial.println("\nparsed json");
-          strcpy(blynk_token, json["blynk_token"]);
+  // Meta Settings
+  configManager.addParameter("version", &meta.version, get);
 
-        } else {
-          Serial.println("failed to load json config");
-        }
-        configFile.close();
-      }
-    }
-  } else {
-    Serial.println("failed to mount FS");
-  }
-  //end read
+  // Init Callbacks
+  configManager.setAPCallback(APCallback);
+  configManager.setAPICallback(APICallback);
 
-  // The extra parameters to be configured (can be either global or just in the setup)
-  // After connecting, parameter.getValue() will get you the configured value
-  // id/name placeholder/prompt default length
-  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
-
-  //WiFiManager
-  //Local intialization. Once its business is done, there is no need to keep it around
-  WiFiManager wifiManager;
-
-  //set config save notify callback
-  wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-  //set static ip
-  //wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-  
-  //add all your parameters here
-  wifiManager.addParameter(&custom_blynk_token);
-
-  //reset settings - for testing
-  //wifiManager.resetSettings();
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  //wifiManager.setMinimumSignalQuality();
-  
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
-  //wifiManager.setTimeout(120);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect("temp-tracker", "123456789")) {
-    Serial.println("failed to connect and hit timeout");
-    delay(3000);
-    //reset and try again, or maybe put it to deep sleep
-    ESP.reset();
-    delay(5000);
-  }
-
-  //if you get here you have connected to the WiFi
-  Serial.println("connected...yeey :)");
-  //read updated parameters
-  strcpy(blynk_token, custom_blynk_token.getValue());
-
-  //save the custom parameters to FS
-  if (shouldSaveConfig) {
-    Serial.println("saving config");
-    DynamicJsonBuffer jsonBuffer;
-    JsonObject& json = jsonBuffer.createObject();
-    json["blynk_token"] = blynk_token;
-
-    File configFile = SPIFFS.open("/config.json", "w");
-    if (!configFile) {
-      Serial.println("failed to open config file for writing");
-    }
-
-    json.printTo(Serial);
-    json.printTo(configFile);
-    configFile.close();
-    //end save
-  }
-
-  Serial.println("local ip");
-  Serial.println(WiFi.localIP());
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println(getTemp());
-}
-
-float getTemp() {
-  uint8_t i;
-  float average;
- 
-  // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES; i++) {
-   samples[i] = analogRead(THERMISTORPIN);
-   delay(10);
-  }
-  
-  // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-     average += samples[i];
-  }
-  average /= NUMSAMPLES;
- 
-  Serial.print("Average analog reading "); 
-  Serial.println(average);
-  
-  // convert the value to resistance
-  average = 1023 / average - 1;
-  average = SERIESRESISTOR / average;
-  float steinhart;
-  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
-  steinhart = 1 / steinhart;                 // Invert
-  steinhart -= 273.15;                         // convert to C
-  
-  Serial.print("Temperature "); 
-  Serial.print(steinhart);
-  Serial.println(" *C");
-  float Tf = (9 / 5) * steinhart + 32; //Celcius to Fahrenheit converstion 
-  return abs(steinhart);  
-}
-
-void handleRoot() {
-  server.send(200, "text/plain", "Temp: " + String(getTemp()));
+  configManager.begin(config);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  server.handleClient();
-  MDNS.update();
-
+  DebugPrintln(F("test"));
+  configManager.loop();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,41 @@
 const path = require("path");
+const fs = require('fs');
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-
 const isProduction = process.env.NODE_ENV === "production";
+const wifiData = require('./scan.json');
 
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: {
     index: "./src/index.js",
-    setup: "./src/setup.js",
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    // filename: "[name].bundle.js",
+    path: isProduction
+      ? path.resolve(__dirname, "./arduino/esp-temp-track/data")
+      : path.resolve(__dirname, "dist"),
   },
   devtool: isProduction ? "" : "inline-source-map",
   devServer: {
     contentBase: "./dist",
     hot: true,
+    before: function(app, server, compiler) {
+      app.get('/scan', function(req, res) {
+        res.json(wifiData);
+      });
+      app.get('/settings', function(req, res) {
+        const data = fs.readFileSync('./settings.json', {encoding: 'utf8'});
+        res.json(JSON.parse(data));
+      });
+      app.post('/settings', function(req, res) {
+        fs.writeFile('./settings.json', JSON.stringify(req.body), err => {
+          if (err) throw err;
+        });
+        res.json(req.body);
+      });
+    }
   },
   plugins: [
     new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
